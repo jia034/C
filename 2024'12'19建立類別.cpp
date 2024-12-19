b697ca55efcb1323c09ccd473aea47b79f6ee670
jia034 <313034@s.lhvs.tyc.edu.tw>
2024-12-19 15:22:45 +0800

    Update 2024'12'19建立類別.cpp

--- 2024'12'19建立類別.cpp
+++ 2024'12'19建立類別.cpp
@@ -7,6 +7,21 @@ class myTime{
 		int minutes;
 		void printTime();
 };
+
+void myTime::printTime(){
+	cout<<hour<<":"<<minutes<<"\n";
+}
 int main(){
+	myTime now, open;
+	now.hour = 8;
+	now.minutes = 18;
+	cout<<"現在時間:";
+	now.printTime();
+	open.hour = 9;
+	open.minutes = 10;
+	cout<<"開門時間:";
+	open.printTime();
+	
 	
 }
+	
